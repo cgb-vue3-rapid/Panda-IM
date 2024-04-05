495978cf6c1b5130a53a116b8bf8e6e9b399f625
QiuTian-324 <1240092443@qq.com>
2024-04-05 16:23:23 +0800

    :card_file_box:data:数据库初始化、连接、迁移

---
+++ service/auth/models/dao/migrate.go
@@ -0,0 +1,18 @@
+package dao
+
+import (
+	"akita/panda-im/service/auth/models/entity"
+	"gorm.io/gorm"
+)
+
+// Migrate 用于执行数据库迁移
+func Migrate(db *gorm.DB) error {
+	// 在这里添加你的表迁移逻辑，例如：
+	err := db.AutoMigrate(
+		&entity.AuthModel{},
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ service/auth/models/dao/vars.go
@@ -0,0 +1,9 @@
+package dao
+
+//type AuthDao struct {
+//	db *gorm.DB
+//}
+//
+//func NewAuthDao(db *gorm.DB) *AuthDao {
+//	return &AuthDao{db: db}
+//}
