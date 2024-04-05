1c2b291885effdf7f90e9770db6d5ce6437d54b5
QiuTian-324 <1240092443@qq.com>
2024-04-05 16:32:27 +0800

    :fire:refactor:移除用户数据表结构

--- service/panda_user/rpc/models/userCommon.go
+++
@@ -1,7 +0,0 @@
-package models
-
-// VerificationQuestion 验证问题、答案
-type VerificationQuestion struct {
-	Question string `gorm:"column:question;comment:'验证问题'" json:"question"` // 验证问题
-	Answer   string `gorm:"column:answer;comment:'验证答案'" json:"answer"`     // 验证答案
-}
