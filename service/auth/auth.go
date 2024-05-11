d1f92d271d4b00451cd6fcaeb2d86a993e274e5a
QiuTian-324 <1240092443@qq.com>
2024-05-11 14:45:29 +0800

    :sparkles:add:认证服务

--- service/auth/auth.go
+++ service/auth/auth.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"akita/panda-im/common/etcdOp"
+	"akita/panda-im/common/xcode"
 	"akita/panda-im/service/auth/internal/config"
 	"akita/panda-im/service/auth/internal/handler"
 	"akita/panda-im/service/auth/internal/svc"
 	"context"
 	"flag"
+	"fmt"
 	"github.com/zeromicro/go-zero/core/logc"
+	"github.com/zeromicro/go-zero/rest/httpx"
 
 	"github.com/zeromicro/go-zero/core/conf"
 	"github.com/zeromicro/go-zero/rest"
@@ -25,8 +29,11 @@ func main() {
 
 	ctx := svc.NewServiceContext(c)
 	handler.RegisterHandlers(server, ctx)
-	//// 自定义错误方法
-	//httpx.SetErrorHandler(xcode.ErrHandler)
-	logc.Infof(context.Background(), "Auth服务启动成功，运行在 %s:%d...\n", c.Host, c.Port)
+	// 上送etcd服务
+	etcdOp.RegisterEtcd(c.ETCD.Endpoints, c.Name, fmt.Sprintf("%s:%d", c.Host, c.Port), c.ETCD.TTL)
+
+	// 自定义错误方法
+	httpx.SetErrorHandler(xcode.ErrHandler)
+	logc.Infof(context.Background(), "AuthAPI服务启动成功，运行在 %s:%d...\n", c.Host, c.Port)
 	server.Start()
 }
